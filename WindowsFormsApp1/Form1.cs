cb544db0f2a46c6592441b627fd8cf1d60fc7f76
VacX_vb15 <VacX_vb15@student.itstep.org>
2022-09-01 14:27:49 +0300

    knopka_v-processe...

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,16 +12,33 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        Random rand = new Random();
+        int x, y, i = 1;
+        Button newBut;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_MouseMove(object sender, MouseEventArgs e)
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
-            button1.Location = new Point(rand.Next(0, this.Size.Width - button1.Width), 
-                rand.Next(0, this.Size.Height - button1.Height));
+            newBut.Width = e.X - newBut.Location.X;
+            newBut.Height = e.Y - newBut.Location.Y;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            x = e.X;
+            y = e.Y;
+            newBut = new Button();
+            newBut.Location = new Point(x, y);
+            newBut.Name = "Button" + i;
+            newBut.Text = "Button" + i++;
+
+            this.Controls.Add(newBut);
         }
     }
 }
