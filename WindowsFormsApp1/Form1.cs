1277b19907d5649838877e7ad09243fbba5db0b8
VacX_vb15 <VacX_vb15@student.itstep.org>
2022-09-01 12:55:52 +0300

    ButtonRun

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            button1.Location = new Point(rand.Next(0, this.Size.Width - button1.Width), 
+                rand.Next(0, this.Size.Height - button1.Height));
+        }
+    }
+}
