460f789da4d4986d587a83a3448ed5ae03186f8f
VacX_vb15 <VacX_vb15@student.itstep.org>
2022-09-02 11:48:52 +0300

    CW_02._2_TimerWork

---
+++ CW-02.2_TimerWork/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CW_02._2_TimerWork
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "0";
+            timer1.Interval = 1;
+        }
+        private int i = 0;
+        private DateTime dt;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            i = (int)(DateTime.Now - dt).TotalMilliseconds;
+            this.Text = i.ToString();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dt = DateTime.Now;
+            timer1.Start();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+    }
+}
