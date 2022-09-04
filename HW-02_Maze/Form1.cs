4a6568ceae2e59fdd3bf3c0a97b79d8ac696c4d9
VacX_vb15 <VacX_vb15@student.itstep.org>
2022-09-05 02:13:34 +0300

    HW_02_Maze_part1_done

---
+++ HW-02_Maze/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace HW_02_Maze
+{
+    public partial class Form1 : Form
+    {
+        private PictureBox pers;
+        private Labirint l;
+        public Form1()
+        {
+            InitializeComponent();
+            Options();
+            StartGame();
+        }
+        public void Options()
+        {
+            Text = "Maze";
+
+            BackColor = Color.FromArgb(255, 92, 118, 137);
+
+            int sizeX = 40;
+            int sizeY = 20;
+
+            Width = sizeX * 16 + 16;
+            Height = sizeY * 16 + 40;
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+        public void StartGame()
+        {
+            l = new Labirint(this, 40, 20);
+            l.Show();
+            pers = new PictureBox();
+            pers = l.images[2, 0];
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A || e.KeyCode == Keys.W || e.KeyCode == Keys.S || e.KeyCode == Keys.D || e.KeyCode == Keys.Left || e.KeyCode == Keys.Up || e.KeyCode == Keys.Down || e.KeyCode == Keys.Right)
+            {
+                if (e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
+                {
+                    if (CheckMove(pers.Location.X - 16, pers.Location.Y))
+                        pers.Location = new Point(pers.Location.X - 16, pers.Location.Y);
+                }
+                else if (e.KeyCode == Keys.W || e.KeyCode == Keys.Up)
+                {
+                    if (CheckMove(pers.Location.X, pers.Location.Y - 16))
+                        pers.Location = new Point(pers.Location.X, pers.Location.Y - 16);
+                }
+                else if (e.KeyCode == Keys.S || e.KeyCode == Keys.Down)
+                {
+                    if (CheckMove(pers.Location.X, pers.Location.Y + 16))
+                        pers.Location = new Point(pers.Location.X, pers.Location.Y + 16);
+                }
+                else if (e.KeyCode == Keys.D || e.KeyCode == Keys.Right)
+                {
+                    if (CheckMove(pers.Location.X + 16, pers.Location.Y))
+                        pers.Location = new Point(pers.Location.X + 16, pers.Location.Y);
+                }
+            }
+        }
+        private bool CheckMove(int x, int y)
+        {
+            x /= 16;
+            y /= 16;
+            if (x >= 0 && y >= 0)
+            {
+                if (l.maze[y,x].type == MazeObject.MazeObjectType.WALL)
+                {
+                    return false;
+                }
+                else if (l.maze[y, x].type == MazeObject.MazeObjectType.ENEMY)
+                {
+                    // -=<< change func GO na Heart-- and then GO >>=- //
+                    GameOver();
+                }
+                else if (l.maze[y, x].type == MazeObject.MazeObjectType.MEDAL)
+                {
+                    // -=<< func get medal++ >>=- //
+                }
+                else if (x == l.width - 1 && y == l.height - 3)
+                {
+                    pers.Location = new Point(x, y);
+                    WIN();
+                }
+                else
+                    return true;
+            }
+            return false;
+        }
+        private void WIN()
+        {
+            MessageBox.Show(" > You won.", ":)", MessageBoxButtons.OK);
+            // -=<< show score >>=- //
+            this.Controls.Clear();
+            Options();
+            StartGame();
+        }
+        private void GameOver()
+        {
+            this.Controls.Clear();
+            MessageBox.Show(" > Congratulations, you have lost))", ":3", MessageBoxButtons.OK);
+            Options();
+            StartGame();
+        }
+    }
+}
---
+++ HW-02_Maze/Labirint.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace HW_02_Maze
+{
+    class Labirint
+    {
+        public int height;
+        public int width;
+
+        public MazeObject[,] maze;
+        public PictureBox[,] images;
+
+        public static Random r = new Random();
+        public Form parent;
+
+        public Labirint(Form parent, int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+            this.parent = parent;
+
+            maze = new MazeObject[height, width];
+            images = new PictureBox[height, width];
+
+            // наш персонажик
+            int x = 0, y = 2;
+            maze[y, x] = new MazeObject(MazeObject.MazeObjectType.CHAR);
+            images[y, x] = new PictureBox();
+            images[y, x].Location = new Point(x * maze[y, x].width, y * maze[y, x].height);
+            images[y, x].Parent = parent;
+            images[y, x].Width = maze[y, x].width;
+            images[y, x].Height = maze[y, x].height;
+            images[y, x].BackgroundImage = maze[y, x].texture;
+            images[y, x].Visible = false;
+
+            Generate();
+        }
+
+        private void Generate()
+        {
+            int smileX = 0;
+            int smileY = 2;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    MazeObject.MazeObjectType current = MazeObject.MazeObjectType.HALL;
+
+                    // в 1 случае из 5 - ставим стену
+                    if (r.Next(5) == 0)
+                        current = MazeObject.MazeObjectType.WALL;
+
+                    // в 1 случае из 250 - кладём денежку
+                    if (r.Next(250) == 0)
+                        current = MazeObject.MazeObjectType.MEDAL;
+
+                    // в 1 случае из 250 - размещаем врага
+                    if (r.Next(250) == 0)
+                        current = MazeObject.MazeObjectType.ENEMY;
+
+                    // стены по периметру обязательны
+                    if (y == 0 || x == 0 || y == height - 1 | x == width - 1)
+                        current = MazeObject.MazeObjectType.WALL;
+
+                    // наш персонажик
+                    if (x == smileX && y == smileY)
+                        continue;
+
+                    // есть выход, и соседняя ячейка справа всегда свободна
+                    if (x == smileX + 1 && y == smileY || x == width - 1 && y == height - 3)
+                        current = MazeObject.MazeObjectType.HALL;
+
+                    maze[y, x] = new MazeObject(current);
+                    images[y, x] = new PictureBox();
+                    images[y, x].Location = new Point(x * maze[y, x].width, y * maze[y, x].height);
+                    images[y, x].Parent = parent;
+                    images[y, x].Width = maze[y, x].width;
+                    images[y, x].Height = maze[y, x].height;
+                    images[y, x].BackgroundImage = maze[y, x].texture;
+                    images[y, x].Visible = false;
+                }
+            }
+        }
+
+        public void Show()
+        {
+            for (int y = 0; y < height; y++)
+                for (int x = 0; x < width; x++)
+                    images[y, x].Visible = true;
+        }
+    }
+}
