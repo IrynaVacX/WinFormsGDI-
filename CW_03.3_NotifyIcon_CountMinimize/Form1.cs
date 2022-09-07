50a208c3b0c589288555bb3dd8e83569cc73bc45
VacX_vb15 <VacX_vb15@student.itstep.org>
2022-09-07 15:18:16 +0300

    CW_03._3_NotifyIcon_CountMinimize

---
+++ CW_03.3_NotifyIcon_CountMinimize/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CW_03._3_NotifyIcon_CountMinimize
+{
+    public partial class Form1 : Form
+    {
+        private int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false;
+        }
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == WindowState)
+            {
+                Hide();
+                notifyIcon1.Text = $"System Tray -=> {++count}";
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(3);
+            }
+        }
+        private void restoreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
