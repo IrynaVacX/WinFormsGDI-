fbf7a8665026e9e4ba0af63727d43231bb524ae2
VacX_vb15 <VacX_vb15@student.itstep.org>
2022-09-09 10:51:41 +0300

    CW_05._2_Screenshot_Timer

---
+++ CW-05.1_RichTextBox/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CW_05._1_RichTextBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            PictureAndSomeText();
+        }
+        private void PictureAndSomeText()
+        {
+            Image img = Image.FromFile("1.png");
+            Clipboard.SetImage(img);
+            richTextBox1.Text += "something text...";
+            richTextBox1.Paste();
+        }
+    }
+}
---
+++ CW-05.2_Screenshot_Timer/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CW_05._2_Screenshot_Timer
+{
+    public partial class Form1 : Form
+    {
+        private static Bitmap bmpScreenshot;
+        private static Graphics gfxScreenshot;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 5000;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Hide();
+            Thread.Sleep(400);
+            bmpScreenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
+            gfxScreenshot = Graphics.FromImage(bmpScreenshot);
+            gfxScreenshot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size, CopyPixelOperation.SourceCopy);
+            bmpScreenshot.Save($"screen/{DateTime.Now.ToString().Replace(':','-')}.jpg", ImageFormat.Jpeg);
+            this.Show();
+        }
+    }
+}
